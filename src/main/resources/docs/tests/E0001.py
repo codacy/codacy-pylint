f0ccec237624f8c14b8c9ce6e0b496e567a548b3
machado <machado@codacy.com>
2015-08-17 12:49:28 +0100

    Added more patterns, some patterns that could not test, were removed. The .md of the done but not tested patterns still exist

---
+++ src/main/resources/docs/tests/E0001.py
@@ -0,0 +1,6 @@
+##Patterns: E0001
+
+##Err: E0001
+class MyClass(object) = {
+    i = 123
+}
---
+++ src/main/resources/docs/tests/R0201.py
@@ -0,0 +1,6 @@
+##Patterns: R0201
+
+class Foo(object):
+##Warn: R0201
+    def bar(self, baz):
+        return 33
