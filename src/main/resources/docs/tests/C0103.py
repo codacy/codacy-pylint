a8f39e72ab6dbb6070435ec8dd4485dc02ad8500
Rafael Cortês <mrfyda@gmail.com>
2018-01-23 18:23:13 +0000

    Add missing class-attribute-rgx parameter

--- src/main/resources/docs/tests/C0103.py
+++ src/main/resources/docs/tests/C0103.py
@@ -15,3 +15,7 @@ class Gname(object):
 class GnaEme(object):
     def good_method(bla):
         print "cool!"
+
+class Complex:
+##Info: C0103
+    THIS_IS_REALLY_LONG_I_MEAN_ITS_MORE_THAN_THIRTY_CHARS = 53
